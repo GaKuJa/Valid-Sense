ba80ccef062992c723e9c3b9a2021ebb3d8f98af
WashinChuen <73007799+WashinChuen@users.noreply.github.com>
2021-11-29 11:51:40 +0900

    Added: Scriptable Objects for: PlayerInfo, LaneInfo, SongInfo, SongResult

--- Valid_Sense/Assets/Member/Washin/PlayerInputTESTING/ScriptableObjectScripts/SongInfo.cs
+++ Valid_Sense/Assets/Member/Washin/PlayerInputTESTING/ScriptableObjectScripts/SongInfo.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "NewTrackInfo",menuName ="ScriptableObjects/TrackInfo")]
+[CreateAssetMenu(fileName = "NewSongInfo",menuName = "WashinScriptableObjects/SongInfo")]
 public class SongInfo : ScriptableObject
 {
+    [Space]
     public string songName;
     public string artistName;
     public int bpm;
     public string chartDesigner;
     public string jacketArt;
+
+    [Space]
     public int songNumberInList;
+    public int easy;
+    public int medium;
+    public int hard;
 }
