83dfd05511d31aa0e231f8ae263d15c242952a9d
Nhelv5388 <20c1020010kf@edu.tech.ac.jp>
2021-12-10 11:59:05 +0900

    SkillEffectManagerの追加

--- Valid_Sense/Assets/Member/kasai/Scripts/SkillEffectManager.cs
+++ Valid_Sense/Assets/Member/kasai/Scripts/SkillEffectManager.cs
@@ -8,34 +8,67 @@ public class SkillEffectManager : MonoBehaviour
     static SkillEffectManager _instance;
 
     [SerializeField] private GameObject SkillEffect;
-    [SerializeField] private GameObject SkillGauge;
-    private GameObject effectobj;
-    private int SkillStats=0;//置き換えあり
+    [SerializeField] private GameObject[] SkillGauge;//スキルゲージ
+    private GameObject effectobj1;
+    private GameObject effectobj2;
+
+    public int SkillStats1=0;//エフェクトのゲージ量    置き換えあり
+    public int SkillStats2=0;//エフェクトのゲージ量    置き換えあり
+
     // Start is called before the first frame update
     private void Awake()
     {
         _instance = this;
-        SkillGauge = GameObject.Find("SkillGauge");
-        effectobj = Instantiate(SkillEffect);
-        effectobj.transform.position = SkillGauge.transform.position;
-        SkillEffect.SetActive(false);
-        SkillStats = 100;//試遊会用
-    }
+        //SkillGauge = GameObject.Find("SkillGauge");
+        effectobj1 = Instantiate(SkillEffect);
+        effectobj1.transform.position = SkillGauge[0].transform.position;
+        effectobj1.gameObject.SetActive(false);
 
-    // Update is called once per frame
+        effectobj2 = Instantiate(SkillEffect);
+        effectobj2.transform.position = SkillGauge[1].transform.position;
+        effectobj2.gameObject.SetActive(false);
+
+        SkillStats1 = 100;//試遊会用
+        SkillStats2 = 100;//試遊会用
+
+    }
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SkillStats1 = 0;
+            SkillStats2 = 0;
+        }
+        if (SkillStats1 == 100)
+        {
+            Debug.Log("true");
+            effectobj1.gameObject.SetActive(true);
+        }
+        else if (SkillStats1 != 100)
+        {
+            Debug.Log("false");
+            effectobj1.gameObject.SetActive(false);
+        }
+
+        if (SkillStats2 == 100)
+        {
+            Debug.Log("true");
+            effectobj2.gameObject.SetActive(true);
+        }
+        else if (SkillStats2 != 100)
+        {
+            Debug.Log("false");
+            effectobj2.gameObject.SetActive(false);
+        }
     }
-    void SkillEffectTrigger()
+
+    IEnumerator Player1Skill()
     {
-        if (SkillStats == 100)
-        {
-            SkillEffect.SetActive(true);
-        }
-        else
-        {
-            SkillEffect.SetActive(false);
-        }
+        effectobj1.gameObject.SetActive(true);
+        yield return null;
+    }
+    IEnumerator Player2Skill()
+    {
+        yield return null;
     }
 }
